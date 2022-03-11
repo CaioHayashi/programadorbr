6d3ec7c6d8e71ef5470a3ce2ddffbab6508a81fe
CaioHayashi <84331740+CaioHayashi@users.noreply.github.com>
2022-03-11 18:42:01 -0300

    adtive

---
+++ JavaScript/Relembrando/script.js
@@ -0,0 +1,23 @@
+/* 
+Operadores lógicos:
+
+    || ou
+    && e
+    ! negativo
+
+Operadores relacionais:
+
+    == igual a
+    === valor é igual e igual
+    != não é igual (é diferente)
+    > maior que 
+    < menor que 
+    >= maior ou igual a
+    <= menor ou igual a 
+
+----------------------------------------------
+ 
+
+Object
+
+var 
