cf1c04118d79e5c30a1a12bf0c9938abf209055c
CaioHayashi <84331740+CaioHayashi@users.noreply.github.com>
2022-03-12 01:00:52 -0300

    keeping

--- JavaScript/Relembrando/script.js
+++ JavaScript/Relembrando/script.js
@@ -1,23 +1,66 @@
 /* 
-Operadores lógicos:
+Object 
 
-    || ou
-    && e
-    ! negativo
+[proriedade do objeto]
 
-Operadores relacionais:
+    var objeto = {               
+        chave: 'valor', 
+        chave: ['valor', 'valor']
+    }
 
-    == igual a
-    === valor é igual e igual
-    != não é igual (é diferente)
-    > maior que 
-    < menor que 
-    >= maior ou igual a
-    <= menor ou igual a 
+    console.log(objeto)
+    ou
+    console.log(objeto['chave'])
+    ou
+    console.log(objeto['chave'][1]) = valor 1
+    
+---------------------------------------------------------
 
-----------------------------------------------
- 
+    adicionando propriedade ao objeto:
 
-Object
+    var objeto['chave'] = valor
 
-var 
+    ou
+
+    var novaChave = 'chave'
+    aluno[novaChave] = valor
+
+    ou
+
+    objeto.chave = valor
+
+   .forEach(function(element){
+       console.log(element)
+   })    
+*/
+
+
+function objNew(n, s){
+    this.nome = n,
+    this.sobrenome = s
+}
+
+a1 = new objNew('Caio', 'Hayashi')
+
+console.log(a1)
+
+
+function obj(n, s){
+    return {
+        nome: n,
+        sobrenome: s
+    }
+}
+
+aluno1 = obj('Coio', 'Hayashi')
+
+console.log(aluno1)
+
+
+
+var a = {
+    nome: 'Caio',
+    sobrenome: 'Hayashi'
+}
+
+console.log(a)
