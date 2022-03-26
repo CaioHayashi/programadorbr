e33425f899a38d1fb69c8f27ac3ec9ff54c75a66
CaioHayashi <84331740+CaioHayashi@users.noreply.github.com>
2022-03-26 11:43:07 -0300

    created ToDoList

---
+++ JavaScript/ToDoLIst/readme.md
@@ -0,0 +1,7 @@
+lista de tarefa:
+
+pegar um elemento a partir do input;
+
+esse valor ser adcionado em uma nova tag;
+
+poder excluir esse elemnto da lista;
---
+++ JavaScript/ToDoLIst/script.js
@@ -0,0 +1,9 @@
+var valuetask = document.querySelector('#valuetask')
+var buttonAdd = document.querySelector('#buttonAdd')
+
+addButton.addEventListener('click' function(){
+    var paragraph = valuetask
+    paragraph.
+
+})
+
