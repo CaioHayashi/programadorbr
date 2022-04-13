8dd2ce1fdd1641474094be81d51eb0dd820904d3
CaioHayashi <84331740+CaioHayashi@users.noreply.github.com>
2022-04-13 15:36:20 -0300

    update

--- HTML/Avançado/video/script.js
+++ HTML/Avançado/video/script.js
@@ -1,31 +1,52 @@
 let video = document.getElementById("video")
+let back = document.getElementById("back")
+let forward = document.getElementById("forward")
+let add = document.getElementById("add")
+let remove = document.getElementById("remove")
+let play = document.getElementById("play")
+let stop = document.getElementById("stop")
+let fullscreen = document.getElementById("fullscreen")
+let progress = document.getElementById("progress")
 
-document.getElementById("back").addEventListener("click", function(){
+back.addEventListener("click", function(){
     video.currentTime -= 15
 })
 
-document.getElementById("forward").addEventListener("click", function(){
+forward.addEventListener("click", function(){
     video.currentTime += 15
 })
 
-document.getElementById("add").addEventListener("click", function(){
+add.addEventListener("click", function(){
     video.playbackRate += 0.1
 })
 
-document.getElementById("remove").addEventListener("click", function(){
+remove.addEventListener("click", function(){
     video.playbackRate -= 0.1
 })
 
-document.getElementById("play").addEventListener("click", function(){
+play.addEventListener("click", function(){
     video.play()
 })
 
-document.getElementById("stop").addEventListener("click", function(){
+stop.addEventListener("click", function(){
     video.pause()
     video.currentTime = 0
     
 })
 
-document.getElementById("fullscreen").addEventListener("click", function(){
+fullscreen.addEventListener("click", function(){
     video.requestFullscreen()
 })
+progress.addEventListener('mousedown', function () {
+    video.pause();
+  });
+  
+progress.addEventListener('change', function () {
+    video.currentTime = progress.value / progress.max * video.duration;
+    video.play();
+  });
+
+video.addEventListener('timeupdate', function () {
+    var position = this.currentTime / this.duration;
+    progress.value = position * 1000;
+  });
