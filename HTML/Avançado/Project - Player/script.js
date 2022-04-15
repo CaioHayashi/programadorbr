9604ae8735b2016756a8f26efd77b939b4e7dbe1
CaioHayashi <84331740+CaioHayashi@users.noreply.github.com>
2022-04-14 23:02:05 -0300

    update

--- HTML/Avançado/Project - Player/script.js
+++ HTML/Avançado/Project - Player/script.js
@@ -1,32 +1,17 @@
-let img = document. getElementById("image")
-let artist = document.getElementById("artist")
-let music = document.getElementById("music")
-let source = document.getElementById("source")
+let names = ["Caio", "Diego", "Deh", "Douglas"]
 
-let musics = [
-    {
-        img: "red",
-        artist: "Clean",
-        music: "Bite Me",
-        src: "assets/Bite Me (Clean) - NEFFEX.mp3",
-    },
-    {
-        img: "blue",
-        artist: "Clean",
-        music: "It's Only Worth It If You Work For It",
-        src: "assets/It's Only Worth It If You Work For It (Clean) - NEFFEX.mp3",
-    },
-    {
-        img: "green",
-        artist: "Clean",
-        music: "Statement",
-        src: "assets/Statement (Clean) - NEFFEX.mp3",
+let index = 0
+function foward() {
+    if( index >= names.length + 1){
+         return index = 0
+    } else {
+        return index ++
     }
-]
-
-img.style.backgroundColor = musics[0].img
-artist.innerHTML = musics[0].artist
-music.innerHTML = musics[0].music
-source.setAttribute("src", musics[0].src)
-
-console.log(musics)
+}
+foward()
+foward()
+foward()
+foward()
+console.log(names.length)
+console.log(index)
+console.log(names[index])
